d08feee1f5612a180957d5aca89b5aab0c01466c
AzkiRH <azkirafsanjani25@gmail.com>
2023-07-01 09:41:58 +0700

    API-2

---
+++ cypress/e2e/3-personal-learn-api/pokeapi/headers.test.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress"/>
+
+describe('Validate Headers', () => {
+    it('Successfully validate content-type', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').its('headers').its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+        cy.get('@pokemon').its('body')
+            .should('not.be.empty')
+    })
+})
