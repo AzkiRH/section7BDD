c78a064340caea8d961ec729a3869606f87970a5
AzkiRH <azkirafsanjani25@gmail.com>
2023-07-14 16:47:17 +0700

    katalonstudio

---
+++ cypress/e2e/EduApp/spymobiletest/Scripts/Tugasakhir/Belajarberhitung/Script1688545205860.groovy
@@ -0,0 +1,74 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\BUDI HERMAWAN\\Katalon Studio\\EduApp\\spymobiletest\\spymobile\\Calculator_v8.1.apk', 
+    true)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.7'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.0'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_plus'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.8'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_equal'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.7'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.8'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_minus'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.4'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_equal'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.7'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.4'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_x'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.2'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_equal'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.1'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.4'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.8'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_divide'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.5'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_equal'), 0)
+
+Mobile.tap(findTestObject('Berhitung/button_no.2'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.9'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_coma'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Berhitung/button_no.6'), 0)
+
+Mobile.closeApplication()
+
---
+++ cypress/e2e/EduApp/spymobiletest/Test Suites/DuoApp.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DuoApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>09b2665c-14b0-4b7c-a743-c1f8d88afc7d</testSuiteGuid>
+   <testCaseLink>
+      <guid>9f5d25bb-44f5-4a73-bc6f-b47fce36edd4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/ChooseTheme</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34f9770b-da9f-47e5-805f-adcd7d6deff4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/CountWithACalculator</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
